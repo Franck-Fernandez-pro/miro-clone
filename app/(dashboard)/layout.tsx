d3fbcfe1571aefce4d6b696e8dd3c671d0fa1894
Franck Fernandez <franck.fernandez@gmail.com>
2024-03-22 16:02:40 +0100

    Import UI components

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,25 @@
+import { Sidebar } from "./_components/sidebar";
+import { Navbar } from "./_components/navbar";
+import { OrgSidebar } from "./_components/org-sidebar";
+import { ReactNode } from "react";
+
+interface DashboardLayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: DashboardLayoutProps) {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          {/* <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div> */}
+        </div>
+      </div>
+    </main>
+  );
+}
